d568da16fe601defa81ffe34e028f727a51c738e
danieltbrad <danieltbrad@gmail.com>
2017-02-07 13:31:21 -0800

    remove auth from products#index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
-    token = request.env["HTTP_AUTHORIZATION"]
-    if token && Auth.decode(token)
+    # token = request.env["HTTP_AUTHORIZATION"]
+    # if token && Auth.decode(token)
       render json: Product.all
-    else
-      render json: { error: { message: "You must have a valid token"}}
-    end
+    # else
+    #   render json: { error: { message: "You must have a valid token"}}
+    # end
   end
 end
